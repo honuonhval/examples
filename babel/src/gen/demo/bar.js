88f8e9badfcd066799e175ebd21dc6c2601bacb2
Alex Dixon <alex@conarrative.com>
2018-04-14 12:59:27 -0700

    Use babel-preset-env

--- babel/src/gen/demo/bar.js
+++ babel/src/gen/demo/bar.js
@@ -1,29 +1,46 @@
-import * as React from "react";
+"use strict";
 
-const MyComponent = ({ key }) => {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.MyComponent = undefined;
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var MyComponent = function MyComponent(_ref) {
+  var key = _ref.key;
+
   console.log('child props', key);
-  return React.createElement(
+  return _react2.default.createElement(
     "li",
     { key: key },
-    "Hello JSX!"
+    "Hello JSX! I'm child number ",
+    key,
+    "!"
   );
 };
 
-const MyDefaultComponent = ({ children }) => React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
+var MyDefaultComponent = function MyDefaultComponent(_ref2) {
+  var children = _ref2.children;
+  return _react2.default.createElement(
+    "div",
     null,
-    "Hello JSX!"
-  ),
-  React.createElement(
-    "ul",
-    null,
-    children
-  )
-);
+    _react2.default.createElement(
+      "h1",
+      null,
+      "Hello JSX!"
+    ),
+    _react2.default.createElement(
+      "ul",
+      null,
+      children
+    )
+  );
+};
 
-export { MyComponent };
-
-export default MyDefaultComponent;
+exports.MyComponent = MyComponent;
+exports.default = MyDefaultComponent;
